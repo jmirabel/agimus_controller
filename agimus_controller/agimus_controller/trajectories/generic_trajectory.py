7c7093e2582fb112999ad6aa87a8ced833de028c
Joseph Mirabel <josephmirabel@gmail.com>
2025-05-28 08:36:47 +0000

    [WIP] Add skeleton for trajectory smoothing in OCP

--- agimus_controller/agimus_controller/trajectories/generic_trajectory.py
+++ agimus_controller/agimus_controller/trajectories/generic_trajectory.py
@@ -1,6 +1,7 @@
 import pinocchio as pin
 import numpy as np
 import numpy.typing as npt
+import abc
 
 from agimus_controller.trajectories.trajectory_base import TrajectoryBase
 from agimus_controller.trajectory import (
@@ -8,6 +9,93 @@ from agimus_controller.trajectory import (
     TrajectoryPointWeights,
     WeightedTrajectoryPoint,
 )
+
+
+class TrajectorySmootherBase:
+    def smooth(
+        self,
+        trajectory: list[TrajectoryPoint],
+        smoothing_indices: None | list[float],
+    ) -> tuple[
+        list[TrajectoryPoint],
+        list[float],
+    ]:
+        if smoothing_indices is None:
+            weights_factor = [
+                1.0,
+            ] * len(trajectory)
+            return trajectory, weights_factor
+        else:
+            return self.perform_smooth(trajectory, smoothing_indices)
+
+    @abc.abstractmethod
+    def perform_smooth(
+        self,
+        trajectory: list[TrajectoryPoint],
+        smoothing_indices: list[float],
+    ) -> tuple[
+        list[TrajectoryPoint],
+        list[float],
+    ]:
+        raise NotImplementedError
+
+
+class _NoSmoothing(TrajectorySmootherBase):
+    def perform_smooth(
+        self,
+        trajectory: list[TrajectoryPoint],
+        smoothing_indices: list[float],
+    ) -> tuple[
+        list[TrajectoryPoint],
+        list[float],
+    ]:
+        weights_factor = [
+            1.0,
+        ] * len(trajectory)
+        return trajectory, weights_factor
+
+
+class TrajectorySmoothingWindowSubsampling(TrajectorySmootherBase):
+    def __init__(
+        self,
+        window: int,
+        subsampling: int,
+        weight_factor: float,
+    ):
+        self.window = window
+        self.subsampling = subsampling
+        self.weight_factor = weight_factor
+
+    def perform_smooth(
+        self,
+        trajectory: list[TrajectoryPoint],
+        smoothing_indices: list[float],
+    ) -> tuple[
+        list[TrajectoryPoint],
+        list[float],
+    ]:
+        new_trajectory = list()
+        weights_factor = list()
+        N = len(trajectory)
+        prev_b = 0
+        for i in smoothing_indices:
+            # a and b are the start (included) and end (excluded) of the interval of
+            # indices
+            a = max(i - self.window, 0)
+            b = min(i + self.window, N)
+            # Add the points that are not changed.
+            new_trajectory.extend(trajectory[prev_b:a])
+            weights_factor.extend([1.0] * (a - prev_b))
+            # Smoothen between a and b
+            shortcut = trajectory[a : self.subsampling : b]
+            new_trajectory.extend(shortcut)
+            weights_factor.extend([self.weight_factor] * len(shortcut))
+            prev_b = b
+
+        # Add the points that are not changed.
+        new_trajectory.extend(trajectory[b:])
+        weights_factor.extend([1.0] * (N - b))
+        return new_trajectory, weights_factor
 
 
 class GenericTrajectory(TrajectoryBase):
@@ -23,13 +111,18 @@ class GenericTrajectory(TrajectoryBase):
         w_pose,
     ):
         super().__init__(ee_frame_name)
-        self.trajectory = None
+        self.trajectory: None | list(TrajectoryPoint) = None
+        self.weights_factor: None | list(float) = None
         self.traj_idx = 0
         self.w_q = w_q
         self.w_qdot = w_qdot
         self.w_qddot = w_qddot
         self.w_robot_effort = w_robot_effort
         self.w_pose = w_pose
+        self._smoother: TrajectorySmootherBase = _NoSmoothing()
+
+    def set_smoothing_trajectory(self, smoother: TrajectorySmootherBase):
+        self._smoother = smoother
 
     def build_trajectory_from_q_dq_ddq_arrays(
         self,
@@ -63,13 +156,21 @@ class GenericTrajectory(TrajectoryBase):
             )
         return trajectory
 
-    def add_trajectory(self, trajectory: list[TrajectoryPoint]) -> None:
+    def add_trajectory(
+        self,
+        trajectory: list[TrajectoryPoint],
+        smoothen_indices: None | list[float],
+    ) -> None:
         """Initialize the trajectory if it wasn't, otherwise extend the trajectory."""
         self.trajectory_is_done = False
+        trajectory, weights_factor = self._smoother.smooth(trajectory, smoothen_indices)
+
         if self.trajectory is None:
             self.trajectory = list(trajectory)
+            self.weights_factor = weights_factor
         else:
             self.trajectory.extend(list(trajectory))
+            self.weights_factor.extend(weights_factor)
 
     def get_traj_point_at_t(self, t: np.float64) -> WeightedTrajectoryPoint:
         traj_point = self.trajectory[self.traj_idx]
